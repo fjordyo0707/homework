0c29e61ec7b954bbdd079788d03648e075481e13
fjordyo0707 <fjordyo0707@gmail.com>
2020-12-09 23:48:22 +0800

    Remove build files

--- hw02/target/rls/debug/deps/hw02-b42c07b5d994fc09.d
+++
@@ -1,6 +0,0 @@
-/home/fjordyo/project/homework/hw02/target/rls/debug/deps/hw02-b42c07b5d994fc09.rmeta: src/lib.rs src/first.rs
-
-/home/fjordyo/project/homework/hw02/target/rls/debug/deps/hw02-b42c07b5d994fc09.d: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
